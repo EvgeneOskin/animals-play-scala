c5ad7c51c791040e18343518589d8e85f266e952
EvgeneOskin <eoskin@crystalnix.com>
2016-04-03 19:20:43 +0600

    Fix config for running tests.

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,20 +1,21 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
+import org.specs2.execute.AsResult
 
 import play.api.test._
 import play.api.test.Helpers._
 
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends PlaySpecification {
 
   "Application" should {
 
-    "send 404 on a bad request" in new WithApplication{
+    "send 404 on a bad request" in new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase()))  {
       route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
     }
 
-    "render the index page" in new WithApplication{
+    "render the index page" in new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase()))  {
       val home = route(FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
--- test/AuthenticationSpec.scala
+++ test/AuthenticationSpec.scala
@@ -1,22 +1,23 @@
 import org.specs2.mutable._
 import org.specs2.runner._
+import org.specs2.execute.AsResult
 import org.junit.runner._
 
 import play.api.test._
 import play.api.test.Helpers._
 
 @RunWith(classOf[JUnitRunner])
-class AuthenticationSpec extends Specification {
+class AuthenticationSpec extends PlaySpecification {
 
   "Authentication" should {
-    "render the login page" in new WithApplication{
+    "render the login page" in new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
       val loginPage = route(FakeRequest(GET, "/login")).get
 
       status(loginPage) must equalTo(OK)
       contentType(loginPage) must beSome.which(_ == "text/html")
       contentAsString(loginPage) must contain ("Login")
     }
-    "process login" in new WithApplication{
+    "process login" in new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
       val loginResponse = route(FakeRequest(POST, "/authenticate/userpass")).get
 
       status(loginResponse) must equalTo(OK)
